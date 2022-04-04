b83367b43c288e3dc64d29981025762111796a0f
Ryan Ghaida <ryan.ghaida@fcb.com>
2022-04-03 20:53:41 -0400

    initial

---
+++ js/animations.js
@@ -0,0 +1,69 @@
+/* This allows the elements to have their default styling if JavaScript is disabled in the browser. */
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+scrollElements.forEach((el) => {
+    el.style.opacity = 0
+  })
+
+const scrollOffset = 100;
+
+
+/* We can detect when an element is in view of the user by determining if the distance of the element from the top of the page is less than the height of the visible part of the page. */
+
+/* We can modify this function to detect when the element has scrolled x pixels into the page via scrollOffset */
+const elementInView = (el, scrollOffset) => {
+    
+    const elementTop = el.getBoundingClientRect().top;
+   
+    return (
+      elementTop <= 
+      ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+    );
+  };
+
+
+  const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+  };
+
+  const hideScrollElement = (element) => {
+    element.classList.remove('scrolled');
+  }
+
+  const handleScrollAnimation = () => {
+      document.querySelectorAll(".js-scroll").forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el);
+    } else {
+        // hideScrollElement(el);
+      }
+  })
+}
+
+  window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  })
+
+
+/* throttle function to optimize performance */
+
+//initialize throttleTimer as false
+let throttleTimer = false;
+ 
+const throttle = (callback, time) => {
+    //don't run the function while throttle timer is true
+    if (throttleTimer) return;
+     
+    //first set throttle timer to true so the function doesn't run
+    throttleTimer = true;
+     
+    setTimeout(() => {
+        //call the callback function in the setTimeout and set the throttle timer to false after the indicated time has passed 
+        callback();
+        throttleTimer = false;
+    }, time);
+}
+
+// window.addEventListener('scroll', () => {
+//     throttle(handleScrollAnimation, 250);
+//   })
---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+
+window.addEventListener('load', function () {
+
+
+   
+
+
+
+
+
+    let bg = document.getElementById("bg");
+    let moon = document.getElementById("moon");
+    let mountain = document.getElementById("mountain");
+    let road = document.getElementById("road");
+    let text = document.getElementById("text");
+    let text2 = document.getElementById("text2");
+    
+    window.addEventListener('scroll', function() {
+        var value = window.scrollY;
+    
+         bg.style.top = -value * .75 + 'px'; 
+        moon.style.left = value  * 1.5 + 'px'; 
+        moon.style.top = value  * 1.5 + 'px'; 
+        mountain.style.top =  85  + 'px'; 
+        road.style.top = value *  .15 + 'px'; 
+        text.style.top = value + 50 * 1 + 'px';
+        text2.style.top = value + 50 * 1 + 'px';
+     
+    
+    });
+  });
+
+
+
+
+  
+
+
